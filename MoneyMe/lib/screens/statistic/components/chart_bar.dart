c0fe3674bd6ed5f988647a9aba63927e886d6813
TaiTrien <v.v.t.trien>
2020-11-19 09:58:26 +0700

    feat: init statistic feat

--- MoneyMe/lib/screens/statistic/components/chart_bar.dart
+++ MoneyMe/lib/screens/statistic/components/chart_bar.dart
@@ -1,9 +1,16 @@
 import 'dart:math';
 
-import 'package:MoneyMe/blocs/tag/tag_bloc.dart';
+import 'package:MoneyMe/blocs/transaction/transaction_bloc.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+
+class Transaction {
+  final String time;
+  final int value;
+
+  Transaction({this.time, this.value, Color color});
+}
 
 class ChartsDemo extends StatefulWidget {
   ChartsDemo() : super();
@@ -15,43 +22,42 @@ class ChartsDemo extends StatefulWidget {
 class ChartsDemoState extends State<ChartsDemo> {
   //
   List<charts.Series> seriesList;
-  static TagBloc _tagBloc;
+  TransactionBloc _transactionBloc;
 
-  static List<charts.Series<Sales, String>> _createRandomData() {
-    final random = Random();
-    final tagsList = _tagBloc.state.tagsList;
-    final expenseData = [
-      Sales('2015', random.nextInt(100)),
-      Sales('2016', random.nextInt(100)),
-      Sales('2017', random.nextInt(100)),
-      Sales('2018', random.nextInt(100)),
-      Sales('2019', random.nextInt(100)),
+  static List<charts.Series<Transaction, String>> _createRandomData() {
+    final expenses = [
+      Transaction(time: '2015', value: 100),
+      Transaction(time: '2016', value: 200),
+      Transaction(time: '2017', value: 300),
+      Transaction(time: '2018', value: 400),
+      Transaction(time: '2019', value: 500),
+      Transaction(time: '2020', value: 800),
     ];
-
-    final revenueData = [
-      Sales('2015', random.nextInt(100)),
-      Sales('2016', random.nextInt(100)),
-      Sales('2017', random.nextInt(100)),
-      Sales('2018', random.nextInt(100)),
-      Sales('2019', random.nextInt(100)),
+    final revenues = [
+      Transaction(time: '2015', value: 100),
+      Transaction(time: '2016', value: 200),
+      Transaction(time: '2017', value: 300),
+      Transaction(time: '2018', value: 400),
+      Transaction(time: '2019', value: 500),
+      Transaction(time: '2020', value: 800),
     ];
 
     return [
-      charts.Series<Sales, String>(
+      charts.Series<Transaction, String>(
         id: 'Sales',
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales,
-        data: expenseData,
-        fillColorFn: (Sales sales, _) {
+        domainFn: (Transaction transaction, _) => transaction.time,
+        measureFn: (Transaction transaction, _) => transaction.value,
+        data: expenses,
+        fillColorFn: (Transaction transaction, _) {
           return charts.MaterialPalette.red.shadeDefault;
         },
       ),
-      charts.Series<Sales, String>(
+      charts.Series<Transaction, String>(
         id: 'Sales',
-        domainFn: (Sales sales, _) => sales.year,
-        measureFn: (Sales sales, _) => sales.sales,
-        data: revenueData,
-        fillColorFn: (Sales sales, _) {
+        domainFn: (Transaction transaction, _) => transaction.time,
+        measureFn: (Transaction transaction, _) => transaction.value,
+        data: revenues,
+        fillColorFn: (Transaction transaction, _) {
           return charts.MaterialPalette.green.shadeDefault;
         },
       ),
@@ -69,7 +75,7 @@ class ChartsDemoState extends State<ChartsDemo> {
         strokeWidthPx: 1.0,
       ),
       domainAxis: charts.OrdinalAxisSpec(
-        renderSpec: charts.NoneRenderSpec(),
+        renderSpec: charts.GridlineRendererSpec(),
       ),
     );
   }
@@ -78,7 +84,7 @@ class ChartsDemoState extends State<ChartsDemo> {
   void initState() {
     super.initState();
     seriesList = _createRandomData();
-    _tagBloc = BlocProvider.of<TagBloc>(context);
+    _transactionBloc = BlocProvider.of<TransactionBloc>(context);
   }
 
   @override
@@ -90,11 +96,4 @@ class ChartsDemoState extends State<ChartsDemo> {
       ),
     );
   }
-}
-
-class Sales {
-  final String year;
-  final int sales;
-
-  Sales(this.year, this.sales);
 }
