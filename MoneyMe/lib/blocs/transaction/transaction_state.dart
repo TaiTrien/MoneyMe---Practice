55473527312f3732c926f2e85b80a704a9a03ef7
TaiTrien <v.v.t.trien>
2020-11-19 11:26:36 +0700

    feat: map data to chart

--- MoneyMe/lib/blocs/transaction/transaction_state.dart
+++ MoneyMe/lib/blocs/transaction/transaction_state.dart
@@ -5,10 +5,20 @@ abstract class TransactionState {
   final List<Transaction> transactionsList;
   final Transaction currentTransaction;
   TransactionState({this.transactionsList, this.currentTransaction});
+
+  get revenues {
+    return transactionsList.where((transaction) => transaction.type == '1').toList();
+  }
+
+  get expenses {
+    return transactionsList.where((transaction) => transaction.type == '2').toList();
+  }
 }
 
 class TransactionblocInitial extends TransactionState {
   TransactionblocInitial() : super(transactionsList: null, currentTransaction: null);
+  get revenues => null;
+  get expenses => null;
 }
 
 class UpdateState extends TransactionState {
--- MoneyMe/lib/screens/statistic/components/chart_bar.dart
+++ MoneyMe/lib/screens/statistic/components/chart_bar.dart
@@ -1,63 +1,76 @@
 import 'dart:math';
 
 import 'package:MoneyMe/blocs/transaction/transaction_bloc.dart';
+import 'package:MoneyMe/models/transaction.dart';
 import 'package:charts_flutter/flutter.dart' as charts;
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
-class Transaction {
+class BarValue {
   final String time;
   final int value;
 
-  Transaction({this.time, this.value, Color color});
+  BarValue({this.time, this.value, Color color});
 }
 
+// ignore: must_be_immutable
 class ChartsDemo extends StatefulWidget {
   ChartsDemo() : super();
+  List<Transaction> expenses;
+  List<Transaction> revenues;
+  TransactionBloc _transactionBloc;
 
   @override
   ChartsDemoState createState() => ChartsDemoState();
 }
 
 class ChartsDemoState extends State<ChartsDemo> {
+  @override
+  void initState() {
+    super.initState();
+    widget._transactionBloc = BlocProvider.of<TransactionBloc>(context);
+    widget.expenses = widget._transactionBloc.state.expenses;
+    widget.revenues = widget._transactionBloc.state.revenues;
+    seriesList = _createRandomData();
+  }
+
   //
   List<charts.Series> seriesList;
-  TransactionBloc _transactionBloc;
 
-  static List<charts.Series<Transaction, String>> _createRandomData() {
-    final expenses = [
-      Transaction(time: '2015', value: 100),
-      Transaction(time: '2016', value: 200),
-      Transaction(time: '2017', value: 300),
-      Transaction(time: '2018', value: 400),
-      Transaction(time: '2019', value: 500),
-      Transaction(time: '2020', value: 800),
-    ];
-    final revenues = [
-      Transaction(time: '2015', value: 100),
-      Transaction(time: '2016', value: 200),
-      Transaction(time: '2017', value: 300),
-      Transaction(time: '2018', value: 400),
-      Transaction(time: '2019', value: 500),
-      Transaction(time: '2020', value: 800),
-    ];
+  List<charts.Series<BarValue, String>> _createRandomData() {
+    final expenses = widget.expenses
+        .map(
+          (expense) => BarValue(
+            time: expense.date,
+            value: int.tryParse(expense.price),
+          ),
+        )
+        .toList();
+    final revenues = widget.revenues
+        .map(
+          (revenue) => BarValue(
+            time: revenue.date,
+            value: int.tryParse(revenue.price),
+          ),
+        )
+        .toList();
     return [
-      charts.Series<Transaction, String>(
-        id: 'Expenses',
-        domainFn: (Transaction transaction, _) => transaction.time.toString(),
-        measureFn: (Transaction transaction, _) => transaction.value,
-        data: expenses,
-        fillColorFn: (Transaction transaction, _) {
-          return charts.MaterialPalette.red.shadeDefault;
+      charts.Series<BarValue, String>(
+        id: 'Revenue',
+        domainFn: (BarValue transaction, _) => transaction.time.toString(),
+        measureFn: (BarValue transaction, _) => transaction.value,
+        data: revenues,
+        fillColorFn: (BarValue transaction, _) {
+          return charts.MaterialPalette.green.shadeDefault;
         },
       ),
-      charts.Series<Transaction, String>(
-        id: 'Revenue',
-        domainFn: (Transaction transaction, _) => transaction.time.toString(),
-        measureFn: (Transaction transaction, _) => transaction.value,
-        data: revenues,
-        fillColorFn: (Transaction transaction, _) {
-          return charts.MaterialPalette.green.shadeDefault;
+      charts.Series<BarValue, String>(
+        id: 'Expenses',
+        domainFn: (BarValue transaction, _) => transaction.time.toString(),
+        measureFn: (BarValue transaction, _) => transaction.value,
+        data: expenses,
+        fillColorFn: (BarValue transaction, _) {
+          return charts.MaterialPalette.red.shadeDefault;
         },
       ),
     ];
@@ -71,38 +84,31 @@ class ChartsDemoState extends State<ChartsDemo> {
       barGroupingType: charts.BarGroupingType.grouped,
       defaultRenderer: charts.BarRendererConfig(
         groupingType: charts.BarGroupingType.grouped,
-        strokeWidthPx: 1.0,
       ),
-      primaryMeasureAxis: new charts.NumericAxisSpec(
-        tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 3),
-      ),
-      secondaryMeasureAxis: new charts.NumericAxisSpec(
-        tickProviderSpec: new charts.BasicNumericTickProviderSpec(desiredTickCount: 3),
-      ),
-      // domainAxis: charts.DateTimeAxisSpec(
-      //   tickFormatterSpec: charts.AutoDateTimeTickFormatterSpec(
-      //     day: charts.TimeFormatterSpec(
-      //       format: 'dd',
-      //       transitionFormat: 'dd MMM',
-      //     ),
+      // domainAxis: new charts.DateTimeAxisSpec(
+      //   tickProviderSpec: charts.DayTickProviderSpec(increments: [1]),
+      //   tickFormatterSpec: new charts.AutoDateTimeTickFormatterSpec(
+      //     day: new charts.TimeFormatterSpec(format: 'EEE', transitionFormat: 'EEE', noonFormat: 'EEE'),
       //   ),
+      //   showAxisLine: false,
       // ),
     );
   }
 
   @override
-  void initState() {
-    super.initState();
-    seriesList = _createRandomData();
-    _transactionBloc = BlocProvider.of<TransactionBloc>(context);
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: barChart(),
+      body: BlocBuilder<TransactionBloc, TransactionState>(
+        builder: (context, state) {
+          return Container(
+            padding: EdgeInsets.all(20.0),
+            child: state.transactionsList.isEmpty
+                ? Center(
+                    child: Text('Bạn chưa có giao dịch nào gần đây'),
+                  )
+                : barChart(),
+          );
+        },
       ),
     );
   }
