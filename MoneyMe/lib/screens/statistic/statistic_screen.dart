2f1a184a663a45f6444a1499743474e1026bf14b
TaiTrien <v.v.t.trien>
2020-12-04 11:09:02 +0700

    fix: ui on statistic screen

--- MoneyMe/lib/screens/statistic/statistic_screen.dart
+++ MoneyMe/lib/screens/statistic/statistic_screen.dart
@@ -60,8 +60,9 @@ class StatisticScreen extends StatelessWidget {
                 ),
               ),
               SizedBox(height: 20.0),
-              Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              Wrap(
+                // mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                runAlignment: WrapAlignment.spaceBetween,
                 children: [
                   Text(
                     'Thống kê theo ngày',
@@ -70,11 +71,13 @@ class StatisticScreen extends StatelessWidget {
                       fontWeight: FontWeight.w400,
                     ),
                   ),
+                  SizedBox(width: 20),
                   TimeRangeDropDown(
                     onPress: controller.setDateRange,
                   ),
                 ],
               ),
+              SizedBox(height: 20),
               BlocBuilder<StatisticBloc, StatisticState>(
                 builder: (context, state) {
                   return FutureBuilder<dynamic>(
